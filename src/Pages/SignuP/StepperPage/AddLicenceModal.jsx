62d314eb4f2bd25672814f93afb3f99c7454707b
AnasPirzada <anaspirzada510@gmail.com>
2023-10-30 19:11:33 +0500

    commit

--- src/Pages/SignuP/StepperPage/AddLicenceModal.jsx
+++ src/Pages/SignuP/StepperPage/AddLicenceModal.jsx
@@ -1,45 +1,120 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
-export const AddLicenceModal = () => {
+import React, { useState } from 'react';
+import { Row } from 'react-bootstrap';
+
+export const AddLicenceModal = ({ onDataSubmit, onHide }) => {
+  const [licenseType, setLicenseType] = useState('');
+  const [licenseNumber, setLicenseNumber] = useState('');
+  const [licenseExpiry, setLicenseExpiry] = useState('');
+  const [licenseStatus, setLicenseStatus] = useState('Active');
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'licenseType') {
+      setLicenseType(value);
+    } else if (name === 'licenseNumber') {
+      setLicenseNumber(value);
+    } else if (name === 'licenseExpiry') {
+      setLicenseExpiry(value);
+    } else if (name === 'licenseStatus') {
+      setLicenseStatus(value);
+    }
+  };
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(file);
+  };
+
+  const handleNextClick = () => {
+    let hasImage = selectedImage ? "Yes" : "No";
+    
+    const data = {
+      licenseType,
+      licenseNumber,
+      licenseExpiry,
+      licenseStatus,
+      selectedImage,
+      hasImage, // Add "hasImage" to data
+    };
+    
+    onDataSubmit(data); // Pass the data to the parent component
+    onHide(); // Close the modal
+  };
+
   return (
-    <div className='w-100'>
-      <Row className='w-100 border-bottom '>
-        <h6>Add Medical License</h6>
+    <div className='w-100 d-flex flex-column justify-content-center align-items-center '>
+      <Row className='border-bottom ms-1' style={{ width: '80%' }}>
+        <h6 className='p-3' style={{ color: '#FAB915' }}>
+          Add Medical License
+        </h6>
       </Row>
-      <Row>
+      <Row className='ms-1' style={{ width: '80%' }}>
         <div style={{ width: '80%' }}>
           <label className='mb-1 ms-3 mt-3 text-muted'>License type:</label>
           <input
+            name="licenseType"
             placeholder='MBBS'
             type='text'
             className='shadow border-0 w-100 m-0 p-2 rounded-5 text-black'
+            onChange={handleChange}
           />
           <label className='mb-1 ms-3 mt-3 text-muted'>License Number:</label>
           <input
+            name="licenseNumber"
             placeholder='16438251798'
             type='text'
             className='shadow border-0 w-100 m-0 p-2 rounded-5 text-black'
+            onChange={handleChange}
           />
           <label className='mb-1 ms-3 mt-3 text-muted'>License Expiry:</label>
           <input
+            name="licenseExpiry"
             placeholder='8/29/2023'
             type='date'
             className='shadow border-0 w-100 m-0 p-2 rounded-5 text-black'
+            onChange={handleChange}
           />
           <label className='mb-1 ms-3 mt-3 text-muted'>License Status:</label>
-          <input
-            placeholder='Active'
-            type='date'
-            className='shadow border-0 w-100 m-0 p-2 rounded-5 text-black'
-          />
+          <select
+            name="licenseStatus"
+            className='shadow border-0 w-100 m-0 p-2 rounded-5 text-black bg-white'
+            onChange={handleChange}
+            value={licenseStatus}
+          >
+            <option>Active</option>
+            <option>InActive</option>
+          </select>
         </div>
       </Row>
-      <Row>
-        <button>Upload ID Image</button>
+      <Row className='ms-1' style={{ width: '80%' }}>
+        {/* Upload ID Image input field */}
+        <input
+          type="file"
+          accept="image/*"
+          className="d-none"
+          onChange={handleImageUpload}
+          id="imageUploadInput"
+        />
+        <label
+          htmlFor="imageUploadInput"
+          className='button border-0 p-3 rounded-5 text-white my-4 ms-3'
+          style={{ width: '30%', cursor: 'pointer' }}
+        >
+          Upload ID Image
+        </label>
       </Row>
-      <Row>
-        <button>Cancel</button>
-        <button>Next</button>
+      <Row
+        className='text-center d-flex justify-content-center '
+        style={{ width: '80%' }}
+      >
+        <button className='p-3 w-25 rounded-5 my-4  backbutton'>Cancel</button>
+        <button
+          className='button border-0 w-25 p-2 rounded-5 text-white my-4 ms-4'
+          onClick={handleNextClick}
+        >
+          Next
+        </button>
       </Row>
     </div>
   );
--- src/Pages/SignuP/StepperPage/Licenandverification.jsx
+++ src/Pages/SignuP/StepperPage/Licenandverification.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
+import AddLicenceModal from './AddLicenceModal';
 import Button from 'react-bootstrap/Button';
-import AddLicenceModal from './AddLicenceModal.jsx';
+import Table from 'react-bootstrap/Table';
+
 export const Licenverification = () => {
   const [show, setShow] = useState(false);
+  const [consoleData, setConsoleData] = useState({});
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const onDataSubmit = (data) => {
+    setConsoleData(data);
+    setIsDataSubmitted(true);
+  };
+
   return (
     <div>
       <Container className='shadow p-4 m-4'>
@@ -27,6 +37,34 @@ export const Licenverification = () => {
             />
           </Col>
         </Row>
+
+        {isDataSubmitted && (
+          <Row className='shadow mt-4'>
+            <Table responsive='sm xl lg md'>
+              <thead>
+                <tr>
+                  <th>license</th>
+                  <th>Number</th>
+                  <th>Expires</th>
+                  <th>Image</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{consoleData.licenseType}</td>
+                  <td>{consoleData.licenseNumber}</td>
+                  <td>{consoleData.licenseExpiry}</td>
+                  <td>
+                    {consoleData.selectedImage ? 'Yes' : 'No'}
+                  </td>
+                  <td>{consoleData.licenseStatus}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Row>
+        )}
+
         <Row className='mt-4'>
           <Col xl={6}>
             <Button
@@ -38,21 +76,20 @@ export const Licenverification = () => {
               Add Medical License
             </Button>
           </Col>
+
           <Col xl={6}>
             <button
               className='border-0 p-4 w-100 rounded-4 text-white text-center '
               style={{ backgroundColor: '#B6C5D1' }}
+              variant='primary'
             >
               Add NPDB Document
             </button>
           </Col>
         </Row>
+
         <Row className='my-5'>
-          <button
-            // variant='contained'
-            className='w-25 m-auto backbutton rounded-5 p-2 bg-transparent '
-          >
-            {/* {activeStep === getSteps().length - 1 ? 'Finish' : 'Next'} */}
+          <button className='w-25 m-auto backbutton rounded-5 p-2 bg-transparent '>
             Back
           </button>
           <button className='w-25 m-auto border-0 rounded-5 p-2 text-white button'>
@@ -61,13 +98,14 @@ export const Licenverification = () => {
         </Row>
       </Container>
       <div className='w-100 bg-white'>
-        <Modal show={show} onHide={handleClose} size='lg'>
+        <Modal show={show} onHide={handleClose} size='xl'>
           <Modal.Body className='w-100'>
-            <AddLicenceModal />
+            <AddLicenceModal onDataSubmit={onDataSubmit} onHide={handleClose}/>
           </Modal.Body>
         </Modal>
       </div>
     </div>
   );
 };
+
 export default Licenverification;
